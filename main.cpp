4d9229a276e2509ac44ed8b4ecb48e3ceda7f66e
Carlo Tomasin <c.tomasin@gmail.com>
2019-06-25 22:59:06 +0200

    std::filesystem

--- main.cpp
+++ main.cpp
@@ -2,9 +2,21 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/syslog_sink.h>
 
+#include <experimental/filesystem>
+#include <iostream>
+
+namespace fs = std::experimental::filesystem;
 
 int main(int argc, const char *argv[]) {
     spdlog::set_level(spdlog::level::trace);
     spdlog::set_default_logger(spdlog::syslog_logger_mt("syslog", "test", LOG_PID));
     spdlog::trace("{} starting", argv[0]);
+
+    fs::path p { argv[1] };
+
+    for (auto item: fs::recursive_directory_iterator( p )) {
+        std::cout << item.path() << (fs::is_character_file(item.path()) ? " C" : " R") << std::endl;
+
+    }
+
 }
